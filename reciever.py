62d36e217ba6af8a87f1cd01689c8f1133f49e27
Kai Fujita <gnfx0514@gmail.com>
2022-02-04 16:57:02 +0900

    :+1:import方法を変更

--- reciever.py
+++ reciever.py
@@ -1,19 +1,21 @@
-from OscServerWrapper import OscServer
+from OscServerWrapper.OscServer import OscBlockingServer
+from OscServerWrapper.OscServer import OscThreadingServer
+from OscServerWrapper.OscServer import OscForkingServer
 
 
 def main():
     # OscBlockingServer
-    # oscServer = OscServer.OscBlockingServer('127.0.0.1', 6700, '/address')
-    # oscServer.startServer()
-    # oscServer.stopServer()
+    oscServer = OscBlockingServer('127.0.0.1', 6700, '/address')
+    oscServer.startServer()
+    oscServer.stopServer()
 
     # OscThreadingServer
-    # oscServer = OscServer.OscThreadingServer('127.0.0.1', 6700, '/address')
+    # oscServer = OscThreadingServer('127.0.0.1', 6700, '/address')
     # oscServer.startThread()
     # oscServer.stopThread()
 
     # OscThreadingServer
-    # oscServer = OscServer.OscForkingServer('127.0.0.1', 6700, '/address')
+    # oscServer = OscForkingServer('127.0.0.1', 6700, '/address')
     # oscServer.startThread()
     # oscServer.wait(5)
     # oscServer.stopThread()
--- sender.py
+++ sender.py
@@ -1,15 +1,16 @@
-from OscClientWrapper import OscClient
+from OscClientWrapper.OscClient import OscClient
+from OscClientWrapper.OscClient import OscSimpleClient
 
 
 def main():
     # OscClient
-    oscClient = OscClient.OscClient('127.0.0.1', 6700, '/address')
+    oscClient = OscClient('127.0.0.1', 6700, '/address')
     oscClient.addMessage('abc')
     oscClient.build()
     oscClient.send()
 
     # OscSimpleClient
-    oscClient = OscClient.OscSimpleClient('127.0.0.1', 6700, '/address')
+    oscClient = OscSimpleClient('127.0.0.1', 6700, '/address')
     oscClient.send('def')
 
 if __name__ == '__main__':
